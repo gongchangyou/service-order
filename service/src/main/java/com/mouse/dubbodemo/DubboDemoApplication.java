b84e24f9e78c45960ad2f26b1f2281f01c29c42f
gongchangyou <gongchangyou@58.com>
2022-05-26 01:09:31 +0800

    rename

--- service/src/main/java/com/mouse/dubbodemo/DubboDemoApplication.java
+++ service/src/main/java/com/mouse/dubbodemo/DubboDemoApplication.java
@@ -1,18 +1,16 @@
 package com.mouse.dubbodemo;
 
-import com.mouse.rocketmq.MessageSource;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 
-@EnableBinding(MessageSource.class)
 @SpringBootApplication(scanBasePackages = {"com.mouse"})
 //@EnableNacosDiscovery
 //@DubboComponentScan(value = "com.braindata.dubbodemo.impl")
-@EnableDubbo(scanBasePackages = "com.braindata.dubbodemo.impl")
-@MapperScan("com.braindata.dubbodemo.repository.db.mapper")
+@EnableDubbo(scanBasePackages = "com.mouse.dubbodemo.impl")
+@MapperScan("com.mouse.dubbodemo.repository.db.mapper")
 public class DubboDemoApplication {
 
     public static void main(String[] args) {
